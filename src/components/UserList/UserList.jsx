51e06265b7473267dfa635a216a4c5bacd765335
anjali-sim <anjalighetia123@gmail.com>
2023-05-18 10:01:27 +0530

    :bug: Added the store and applied middleware

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchUsers } from "../../action/userAction";
 import UserListStyle from "@styled/UserList";
 import UserCard from "@UserList/UserCard";
-import USERLISTDATA from "@constants/data";
 import Navbar from "@Navbar/Navbar";
 
-function UserList() {
+function UserList({ loading, users, error, fetchUsers }) {
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
     <UserListStyle>
       <Navbar />
-      {USERLISTDATA.map((user) => (
+      {users.map((user) => (
         <UserCard
           key={user.id}
           id={user.id}
-          profile={user.profile}
-          firstName={user.firstName}
-          lastName={user.lastName}
+          profile={user.avatar}
+          firstName={user.first_name}
+          lastName={user.last_name}
           email={user.email}
         />
       ))}
@@ -22,4 +35,15 @@ function UserList() {
   );
 }
 
-export default UserList;
+const mapStateToProps = (state) => ({
+  loading: state.loading,
+  users: state.users,
+  error: state.error,
+});
+
+const mapDispatchToProps = {
+  fetchUsers,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserList);
+
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import GlobalStyle from "@styled/GlobalStyle";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import userReducer from "./reducer/userReducer.jsx";
+
+const store = createStore(userReducer, applyMiddleware(thunk));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
